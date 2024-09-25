4c573b1df1612c65de11ba917a33957b0ec0f616
Alishba Waris <alishbacreative@gmail.com>
2024-09-25 13:30:12 +0500

    Complete designing of Clothes, Bags and Shoes pages

--- src/components/pages/Clothes.js
+++ src/components/pages/Clothes.js
@@ -1,8 +1,203 @@
 import React from 'react'
+import p_img39 from "../../assets/images/p_img39.png";
+import p_img16 from "../../assets/images/p_img16.png";
+import p_img38 from "../../assets/images/p_img18.png";
+import p_img20 from "../../assets/images/p_img20.png";
+import p_img19 from "../../assets/images/p_img19.png";
+import p_img32 from "../../assets/images/p_img32.png";
+import p_img22 from "../../assets/images/p_img22.png";
+import p_img26 from "../../assets/images/p_img26.png";
+import p_img27 from "../../assets/images/p_img27.png";
+import p_img28 from "../../assets/images/p_img28.png";
+import p_img29 from "../../assets/images/p_img29.png";
+import p_img31 from "../../assets/images/p_img31.png";
+
+
 
 const Clothes = () => {
   return (
-    <div>Clothes</div>
+    <>
+      <div>
+        <div>
+          <h2 className='new_arrival'>Ready to Wear</h2>
+        </div>
+        <div className='line'></div>
+      </div>
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img39} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,299</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img16} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,500</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img38} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,509</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img20} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 2,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img19} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,999</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img29} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,799</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img22} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img32} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img26} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img27} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,500</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img28} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 3,099</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={p_img31} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 2,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </>
   )
 }
 
--- src/components/pages/Shoes.js
+++ src/components/pages/Shoes.js
@@ -1,8 +1,201 @@
 import React from 'react'
+import shoe from "../../assets/images/shoe.webp";
+import shoe1 from "../../assets/images/shoe1.webp";
+import shoe6 from "../../assets/images/shoe6.webp";
+import shoe3 from "../../assets/images/shoe3.webp";
+import shoe2 from "../../assets/images/shoe2.webp";
+import shoe4 from "../../assets/images/shoe4.webp";
+import shoe5 from "../../assets/images/shoe5.webp";
+import shoe9 from "../../assets/images/shoe9.webp";
+import shoe8 from "../../assets/images/shoe8.webp";
+import shoe7 from "../../assets/images/shoe7.webp";
+import shoe10 from "../../assets/images/shoe7.webp";
+
 
 const Shoes = () => {
   return (
-    <div>Shoes</div>
+    <>
+
+      <div>
+        <div>
+          <h2 className='new_arrival'>Foot Wear</h2>
+        </div>
+        <div className='line'></div>
+      </div>
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe3} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,299</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe2} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,500</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe4} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,509</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe5} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 2,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe7} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,999</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe6} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,799</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe1} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe9} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='margin'></div>
+
+      <div className="row row-cols-1 row-cols-md-3 g-4 w-75 m-auto">
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe8} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 1,500</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe6} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 3,099</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img src={shoe10} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Rs. 2,599</h5>
+              <p className="card-text"> Find The Perfect Style at Unbelievable Prices! </p>
+            </div>
+            <div class="card-body">
+              <a href="/" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </>
   )
 }
 
