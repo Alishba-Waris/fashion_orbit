bd1289f87d388cd37886024dcdf04bdda51a9828
Alishba Waris <alishbacreative@gmail.com>
2024-11-04 15:16:21 +0500

    Implement subscription functionality in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,10 +4,29 @@ import { NavLink, Link } from "react-router-dom";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    if (!isSubscribed) {
+      toast.success("Congratulations! You've Subscribed to fashionOrbit");
+      setIsSubscribed(true);
+    } else {
+      toast.info("You've Unsubscribed FashionOrbit");
+      setIsSubscribed(false);
+      setEmail("");
+    }
+  };
+
   return (
     <>
+      <ToastContainer />
       <div className="margin"></div>
       <footer className="footer">
         <div className="footer-container">
@@ -109,9 +128,25 @@ const Footer = () => {
             <div className="footer-column">
               <h4>Subscribe to FashionOrbit</h4>
               <p>Monthly digest of what's new and exciting from us.</p>
-              <form>
-                <input type="email" placeholder="Email address" required />
-                <button type="submit">Subscribe</button>
+              <form onSubmit={handleSubscribe}>
+                <input
+                  type="email"
+                  placeholder={isSubscribed ? email : "Email address"}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  disabled={isSubscribed}
+                />
+
+                <button
+                  type="submit"
+                  style={{
+                    backgroundColor: isSubscribed ? "red" : "#007bff",
+                    color: "white",
+                  }}
+                >
+                  {isSubscribed ? "Unsubscribe" : "Subscribe"}
+                </button>
               </form>
             </div>
           </div>
